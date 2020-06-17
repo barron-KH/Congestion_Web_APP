6bbf000dee498bc1c8409365c27fe9feed4301aa
Barron <barronkhalil100@gmail.com>
2020-06-17 03:21:09 +0100

    create footer

---
+++ leave_managment/footer.php
@@ -0,0 +1,20 @@
+<div class="clearfix"></div>
+         <footer class="site-footer">
+            <div class="footer-inner bg-white">
+               <div class="row">
+                  <div class="col-sm-6">
+                     Copyright &copy; 2018 KH.B
+                  </div>
+                  <div class="col-sm-6 text-right">
+                     Designed by <a>KH</a>
+                  </div>
+               </div>
+            </div>
+         </footer>
+      </div>
+      <script src="src/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
+      <script src="src/js/popper.min.js" type="text/javascript"></script>
+      <script src="src/js/plugins.js" type="text/javascript"></script>
+      <script src="src/js/main.js" type="text/javascript"></script>
+   </body>
+</html>
